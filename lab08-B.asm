62b1ecf4d7e1a7685c5681e93f5b97f8383240e8
powcoder代写 <powcoder@163.com>
2022-07-02 13:07:56 +0800

    程序代写 加微信 powcoder

---
+++ lab08-B.asm
@@ -0,0 +1,17 @@
+	.data
+
+S1:	.asciiz "In what year were you born? "
+S2:	.asciiz "What year is it now? "
+S3:	.asciiz "You will turn "
+S4: 	.asciiz " years old this year.\n"
+SPACE:	.asciiz " "
+NL:	.asciiz "\n"
+	
+	.text
+main:
+	la $a0, S4
+	addi $v0, $zero, 4
+	syscall	
+				
+	addi $v0, $zero, 10
+	syscall
